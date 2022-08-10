7ab865b42ce10d9460fa90911a74eb9954706f08
Michael Pesce <michaelcpesce@gmail.com>
2022-08-10 10:48:53 -0400

    set up e2e testing

---
+++ electron/cypress/e2e/Homepage.cy.js
@@ -0,0 +1,23 @@
+describe('homepage', () => {
+    it('test that homepage loads properly', () => {
+        //load webpage
+        cy.visit('/')
+
+        //locate header logo
+        cy.findByRole('img', { name: /pareto logo/i })
+
+        //locate heading
+        cy.findByRole('heading', {  name: /scenarios/i})
+
+        //locate new scenario button
+        cy.findByRole('button', { name: /create new scenario/i})
+
+        //locate table headers
+        cy.findByRole('columnheader', {  name: /scenario name/i})
+        cy.findByRole('columnheader', {  name: /date created/i})
+        cy.findByRole('columnheader', {  name: /status/i})
+        cy.findByRole('columnheader', {  name: /actions/i})
+        
+        cy.screenshot('end-test')
+    })
+})
