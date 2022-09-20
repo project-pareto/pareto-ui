43cf02bbae7ce5b4aa14fd8ca195735d05d21127
MichaelPesce <michaelcpesce@gmail.com>
2022-09-20 14:58:51 -0400

    changed data directory to home/.pareto-ui

--- backend/app/internal/scenario_handler.py
+++ backend/app/internal/scenario_handler.py
@@ -4,14 +4,10 @@ import io
 import os
 import datetime
 import math
+from pathlib import Path
 from .get_data import get_data, get_input_lists
 
 _log = logging.getLogger(__name__)
-# logging.basicConfig(filename="backend_logs.log",
-#                     filemode='a',
-#                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-#                     datefmt='%H:%M:%S',
-#                     level=logging.DEBUG)
 _h = logging.StreamHandler()
 _h.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
 _log.addHandler(_h)
@@ -21,32 +17,35 @@ _log.setLevel(logging.DEBUG)
 
 class ScenarioHandler:
     def __init__(self) -> None:
+
         _log.info(f"initializing scenario handler")
         self.scenario_list = []
-        self.data_directory_path = "ParetoUI_Data/"
+        self.data_directory_path = Path.home() / ".pareto-ui"
         self.pareto_data_path = os.path.join(self.data_directory_path, "pareto_data.json")
         self.scenarios_path = os.path.join(self.data_directory_path, "scenarios.json")
-        self.excelsheets_path = os.path.join(self.data_directory_path, "excelsheets/")
-        self.outputs_path = os.path.join(self.data_directory_path, "outputs/")
+        self.excelsheets_path = self.data_directory_path / "excelsheets"
+        self.outputs_path = self.data_directory_path / "outputs"
+
         _log.info(f"app directory: {os.path.dirname(os.path.abspath(__file__))}")
         _log.info(f"currently operating in directory: {os.getcwd()}")
         try:
             _log.info(f"changing to home directroy")
-            home_dir = os.path.expanduser("~")
-            # app_dir = app_dir.split('ParetoUI')[0]
+            home_dir = Path.home()
             _log.info(f"new directory: {home_dir}")
             os.chdir(home_dir)
         except Exception as e:
             _log.info(f"unable to change to app directroy: {e}")
-        if not os.path.exists(self.data_directory_path):
-            _log.info(f"making directory: {self.data_directory_path}")
-            os.makedirs(self.data_directory_path) 
-        if not os.path.exists(self.excelsheets_path):
-            _log.info(f"making directory: {self.excelsheets_path}")
-            os.makedirs(self.excelsheets_path) 
-        if not os.path.exists(self.outputs_path):
-            _log.info(f"making directory: {self.outputs_path}")
-            os.makedirs(self.outputs_path) 
+
+        # create data directories
+        _log.info(f"making directory: {self.data_directory_path}")
+        self.data_directory_path.mkdir(parents=True, exist_ok=True)
+
+        _log.info(f"making directory: {self.excelsheets_path}")
+        self.excelsheets_path.mkdir(parents=True, exist_ok=True)
+
+        _log.info(f"making directory: {self.outputs_path}")
+        self.outputs_path.mkdir(parents=True, exist_ok=True)
+
         self.retrieve_scenarios()
 
     def retrieve_scenarios(self):
@@ -67,13 +66,6 @@ class ScenarioHandler:
     
     def upload_excelsheet(self, output_path, filename):
         _log.info(f"Uploading excel sheet: {filename}")
-
-        # get default set and parameter lists
-        # f = open(self.pareto_data_path,'r')
-        # data = json.load(f)
-        # set_list = data['set_list']
-        # parameter_list = data['parameter_list']
-        # f.close()
 
         [set_list, parameter_list] = get_input_lists()
 
