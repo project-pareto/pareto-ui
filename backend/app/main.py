f73bef8863265ef94c953492e187496e88ee91c5
MichaelPesce <michaelcpesce@gmail.com>
2022-09-23 07:48:57 -0400

    0.0.0.0 -> 127.0.0.1

--- backend/app/main.py
+++ backend/app/main.py
@@ -92,4 +92,4 @@ if __name__ == '__main__':
     else:
         _log.info(f"\nstarting app!!")
         multiprocessing.freeze_support()
-        uvicorn.run(app, host="0.0.0.0", port=8001, reload=False)
+        uvicorn.run(app, host="127.0.0.1", port=8001, reload=False)
