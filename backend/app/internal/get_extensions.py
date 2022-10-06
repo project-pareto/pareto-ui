45348bd25ccbb1b6774918a16c10838a93264c6c
MichaelPesce <michaelcpesce@gmail.com>
2022-10-06 10:02:48 -0400

    idaes extensions installer update

--- backend/app/internal/get_extensions.py
+++ backend/app/internal/get_extensions.py
@@ -1,9 +1,8 @@
 from pathlib import Path
-from idaes.util.download_bin import download_binaries
-from idaes.config import default_binary_release
 import sys
 import os
 from shutil import copytree
+import certifi
 
 idaes_extensions_dir = Path.home() / ".pareto" / ".idaes"
 
@@ -18,14 +17,21 @@ def get_idaes_extensions():
     print('inside get idaes extensions')
     try:
         if(sys.platform == "darwin"):
+            print('mac')
             idaes_src = Path(os.path.dirname(os.path.realpath(__file__))) / 'extensions' / '.idaes'
             idaes_dst = Path.home() / ".idaes"
-            print('mac')
+            print(f'moving binaries from {idaes_src} to {idaes_dst}')
+            copytree(idaes_src,idaes_dst,dirs_exist_ok=True)
+            print(f'get idaes extensions successful, making directory')
+        elif(sys.platform == "linux"):
+            print('linux')
+            idaes_src = Path(os.path.dirname(os.path.realpath(__file__))) / 'extensions' / '.idaes'
+            idaes_dst = Path.home() / ".idaes"
             print(f'moving binaries from {idaes_src} to {idaes_dst}')
             copytree(idaes_src,idaes_dst,dirs_exist_ok=True)
             print(f'get idaes extensions successful, making directory')
         else:
-            print('not mac')
+            print('windows')
             try:
                 print(f'setting requests_ca_bundle and ssl_cert_file to certifi.where(): {certifi.where()}')
                 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
@@ -33,7 +39,7 @@ def get_idaes_extensions():
             except Exception as e:
                 print(f'unable to set requests_ca_bundle and ssl_cert_file:\n{e}')
             print(f'trying to download binaries')
-            from idaes.commands.util.download_bin import download_binaries
+            from idaes.util.download_bin import download_binaries
             from idaes.config import default_binary_release
             download_binaries(release=default_binary_release)
             print(f'extensions have been gotten')
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,9 @@
 import sys
 import os
-import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import multiprocessing
-import certifi
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -37,12 +35,6 @@ async def root():
 
 if __name__ == '__main__':
     if('i' in sys.argv or 'install' in sys.argv):
-        try:
-            _log.info(f'setting requests_ca_bundle and ssl_cert_file to certifi.where(): {certifi.where()}')
-            os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
-            os.environ["SSL_CERT_FILE"] = certifi.where()
-        except Exception as e:
-            _log.info(f'unable to set requests_ca_bundle and ssl_cert_file:\n{e}')
         _log.info('running get_extensions()')
         if not check_for_idaes_extensions():
             get_idaes_extensions()
