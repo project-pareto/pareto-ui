07dbcd0cd546c4210ce8137f97cfa1d202cdbcc3
MichaelPesce <michaelcpesce@gmail.com>
2022-10-19 08:23:07 -0400

    added initial sankey node filtering

--- electron/ui/src/views/ModelResults/SankeyPlot.js
+++ electron/ui/src/views/ModelResults/SankeyPlot.js
@@ -14,34 +14,58 @@ import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import ListItemIcon from '@mui/material/ListItemIcon';
 
-
 export default function SankeyPlot(props) {
     const [ sankeyCategory, setSankeyCategory ] = useState("v_F_Piped")
-    const [ filteredNodes, setFilteredNodes ] = useState(["CP01", "F03", "N21", "N12"]) 
-    // const [ filteredNodes, setFilteredNodes ] = useState(["CP01"])
-    const [ totalNodes, setTotalNodes ] = useState(["CP01", "F03", "N21", "N12", "CP02"])
+    const [ filteredNodes, setFilteredNodes ] = useState([]) 
+    const [ totalNodes, setTotalNodes ] = useState([])
     const isAllSelected = totalNodes.length > 0 && filteredNodes.length === totalNodes.length;
     const [ plotData, setPlotData ] = useState({link: {source: [], target:[], value: [], label: []}, node: {label: []}});
     
     useEffect(()=>{
-        unpackData(false);
+        unpackData(true, []);
     }, [sankeyCategory]);
 
     const handleCategoryChange = (event) => {
         setSankeyCategory(event.target.value)
     }
 
-    const unpackData = (unpackAll) =>
+    const handleNodeFilter = (node) => {
+        var tempNodes
+        if (node === 'all') {
+            tempNodes = filteredNodes.length === totalNodes.length ? [] : totalNodes
+            setFilteredNodes(tempNodes);
+        }
+        else {
+            tempNodes = [...filteredNodes]
+            const index = tempNodes.indexOf(node);
+            if (index > -1) { // only splice array when item is found
+                tempNodes.splice(index, 1); // 2nd parameter means remove one item only
+            } else{
+                tempNodes.push(node)
+            }
+            setFilteredNodes(tempNodes)
+        }
+        unpackData(false, tempNodes)
+    }
+
+    const unpackData = (unpackAll, filtered) =>
     {
+        if(!unpackAll) var tempFilteredNodes = filtered
+        else var tempFilteredNodes = filteredNodes
+        console.log('unpacking data with unpackall = '+unpackAll)
         var d = {link: {source: [], target:[], value: [], label: []}, node: {label: []}}
         var locationsInArray = {}
+        var nodeSet = new Set()
         for (let i = 1; i < props.data[sankeyCategory].length; i++) {
-            
             var source = props.data[sankeyCategory][i][0]
             var target = props.data[sankeyCategory][i][1]
             var label = props.data[sankeyCategory][i][2]
             var value = props.data[sankeyCategory][i][3]
-            // if (unpackAll || filteredNodes.includes(source) || filteredNodes.includes(target)) {
+
+            nodeSet.add(source)
+            nodeSet.add(target)
+
+            if (unpackAll || tempFilteredNodes.includes(source) || tempFilteredNodes.includes(target)) {
                 var sourceIndex, targetIndex
                 if (d.node.label.includes(source)) {
                     sourceIndex = locationsInArray[source]
@@ -59,13 +83,15 @@ export default function SankeyPlot(props) {
                     d.node.label.push(target)
                     locationsInArray[target] = targetIndex
                 }
-                // console.log('label for '+source+' going to '+target+' is '+label+'. value is '+value)
                 d.link.source.push(sourceIndex)
                 d.link.target.push(targetIndex)
                 d.link.value.push(value)
                 d.link.label.push(label)
-            // }
+            }
+            
         }
+        setTotalNodes(Array.from(nodeSet))
+        if(unpackAll) setFilteredNodes(Array.from(nodeSet))
         console.log(d)
         setPlotData(d)
           
@@ -133,8 +159,8 @@ export default function SankeyPlot(props) {
                 <AccordionSummary>
                 <p style={{margin:0, fontWeight: "bold", color: "#0884b4"}}>Time & Location Filters</p>
                 </AccordionSummary>
-                <AccordionDetails>
-                    <MenuItem value="all">
+                <AccordionDetails sx={{ height:"500px", overflow: "scroll"}}>
+                    <MenuItem value="all" onClick={()=> handleNodeFilter("all")}>
                     <ListItemIcon>
                         <Checkbox
                         checked={isAllSelected}
@@ -147,8 +173,8 @@ export default function SankeyPlot(props) {
                         primary="Select All"
                     />
                     </MenuItem>
-                    {totalNodes.map((option) => (
-                    <MenuItem key={option} value={option}>
+                    {totalNodes.map((option, index) => (
+                    <MenuItem key={option} value={option} onClick={()=> handleNodeFilter(option)}>
                         <ListItemIcon>
                         <Checkbox checked={filteredNodes.indexOf(option) > -1} />
                         </ListItemIcon>
