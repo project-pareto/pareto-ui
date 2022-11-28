45e485aa20cc9b664ae1d68a60f43268f943fec3
MichaelPesce <michaelcpesce@gmail.com>
2022-11-28 15:28:12 -0500

    updated KPI, Disposal -> Annual Disposal

--- electron/ui/src/views/ModelResults/KPIDashboard.js
+++ electron/ui/src/views/ModelResults/KPIDashboard.js
@@ -101,7 +101,7 @@ export default function KPIDashboard(props) {
             <Grid item xs={12}>
                 <Box sx={{display: 'flex', justifyContent: 'center'}}>
                 <IconButton disabled><RemoveCircleOutlineIcon sx={{ color: "#fc2414" }}/></IconButton>
-                <p style={styles.kpiTitle}>Disposal</p>
+                <p style={styles.kpiTitle}>Annual Disposal</p>
                 </Box>
             </Grid>
             <Grid item xs={12}>
