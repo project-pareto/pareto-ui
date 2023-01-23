10e9ec9d5654da9eaf8e27d7c8e66579034dc971
MichaelPesce <michaelcpesce@gmail.com>
2023-01-23 09:26:39 -0600

    added column filtering for model results, also replaced table with datatable component

--- electron/ui/src/views/ModelResults/ModelResults.js
+++ electron/ui/src/views/ModelResults/ModelResults.js
@@ -1,27 +1,64 @@
 import React from 'react';
-import {useEffect} from 'react';   
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import TableContainer from '@mui/material/TableContainer';
-import LinearProgress from '@mui/material/LinearProgress';
+import {useEffect, useState} from 'react';   
+import { Table, TableBody, TableCell, TableHead, TableRow, TableContainer, Box, Grid, LinearProgress } from '@mui/material';
 import SankeyPlot from './SankeyPlot';
 import KPIDashboard from './KPIDashboard';
-import demoOutputDiagram from "../../assets/demo_figure_output.png";
 import ParetoDictionary from '../../assets/ParetoDictionary.json'
 import NetworkDiagram from '../../components/NetworkDiagram/NetworkDiagram';
+import DataTable from '../../components/DataTable/DataTable';
+import FilterDropdown from '../../components/FilterDropdown/FilterDropdown';
 
 
 export default function ModelResults(props) {
-  const scenario = props.scenario
-   useEffect(()=>{
-      //  console.log('curr scenario.results.data',scenario.results.data)
-   }, [scenario]);
+  const [ scenario, setScenario] = useState({...props.scenario})
+  const [ columnNodesMapping, setColumnNodesMapping ] = useState([]) 
+  const [ columnNodes, setColumnNodes ] = useState([])
+  const [ filteredColumnNodes, setFilteredColumnNodes ] = useState([])
+  const [ rowNodesMapping, setRowNodesMapping ] = useState([]) 
+  const [ rowNodes, setRowNodes ] = useState([])
+  const [ filteredRowNodes, setFilteredRowNodes ] = useState([])
+  const isAllColumnsSelected = columnNodesMapping.length > 0 && filteredColumnNodes.length === columnNodesMapping.length;
+  const isAllRowsSelected = rowNodesMapping.length > 0 && filteredRowNodes.length === rowNodesMapping.length;
+  useEffect(()=>{
+    /*
+      when category is changed, reset the nodes for filtering (columns and rows of current table)
+    */
+    try {
+      if (props.category !== "Dashboard" && props.category !== "Network Diagram" && props.category !== "Sankey") {
+        console.log("inside modelresults useeffect")
+        let tempColumnNodes = {}
+        let tempColumnNodesMapping = []
+        let tempRowNodes = {}
+        let tempRowNodesMapping = []
+        for (let ind in scenario.results.data[props.category][0]) {
+          let columnNode = `${ind}::${scenario.results.data[props.category][0][ind]}`
+          tempColumnNodesMapping.push(columnNode)
+          tempColumnNodes[columnNode] = true
+        }
+        console.log(tempColumnNodes)
+        let i = 0
+        for (let each of scenario.results.data[props.category].slice(1)) {
+          let rowNode = `${i}::${each[0]}`
+          tempRowNodesMapping.push(rowNode)
+          tempRowNodes[rowNode] = true
+          i+=1
+        }
+        console.log(tempRowNodes)
+        setColumnNodes(tempColumnNodes)
+        setRowNodes(tempRowNodes)
+        setFilteredColumnNodes(tempColumnNodesMapping)
+        setFilteredRowNodes(tempRowNodesMapping)
+        setColumnNodesMapping(tempColumnNodesMapping)
+        setRowNodesMapping(tempRowNodesMapping) 
+      }
+    } catch (e) {
+      console.error('unable to set filtering data: ',e)
+    }
+    let tempScenario = {}
+    Object.assign(tempScenario, props.scenario);
+    setScenario(tempScenario)
+    
+  }, [props.category, props.scenario, scenario.data_input.df_parameters]);
 
    const styles ={
     firstCol: {
@@ -35,26 +72,66 @@ export default function ModelResults(props) {
     }
   }
 
-  function toFixed(value, precision) {
-    if(typeof(value) === "string") {
-      try {
-        value = parseInt(value)
-      }catch (e) {
-        console.log('unable to fix decimals for value: ',value)
-        return value
+  const handleColumnFilter = (col) => {
+    var tempCols
+    let tempColumnNodes = {...columnNodes}
+    if (col === 'all') {
+      tempCols = filteredColumnNodes.length === columnNodesMapping.length ? [] : columnNodesMapping
+      if (filteredColumnNodes.length === columnNodesMapping.length) {
+        for (const key of Object.keys(tempColumnNodes)) {
+          tempColumnNodes[key] = false
+        }
+      } else {
+        for (const key of Object.keys(tempColumnNodes)) {
+          tempColumnNodes[key] = true
+        }
       }
+      setColumnNodes(tempColumnNodes)
+      setFilteredColumnNodes(tempCols);
     }
-    precision = precision || 0
-    var power = Math.pow(10, precision),
-      absValue = Math.abs(Math.round(value * power)),
-      result = (value < 0 ? '-' : '') + String(Math.floor(absValue / power));
+    else {
+      tempCols = [...filteredColumnNodes]
+      const index = tempCols.indexOf(col);
+      if (index > -1) { // only splice array when item is found
+        tempCols.splice(index, 1); // 2nd parameter means remove one item only
+      } else{
+        tempCols.push(col)
+      }
+      tempColumnNodes[col] = !tempColumnNodes[col]
+      setColumnNodes(tempColumnNodes)
+      setFilteredColumnNodes(tempCols)
+    }
+}
 
-    if (precision > 0) {
-        var fraction = String(absValue % power),
-            padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
-        result += '.' + padding + fraction;
+const handleRowFilter = (row) => {
+    var tempRows
+    let tempRowNodes = {...rowNodes}
+    if (row === 'all') {
+      tempRows = filteredRowNodes.length === rowNodesMapping.length ? [] : rowNodesMapping
+      if (filteredRowNodes.length === rowNodesMapping.length) {
+        for (const key of Object.keys(tempRowNodes)) {
+          tempRowNodes[key] = false
+        }
+      } else {
+        for (const key of Object.keys(tempRowNodes)) {
+          tempRowNodes[key] = true
+        }
+      }
+      setRowNodes(tempRowNodes)
+      setFilteredRowNodes(tempRows);
     }
-    return result;
+    else {
+      tempRows = [...filteredRowNodes]
+        const index = tempRows.indexOf(row);
+        if (index > -1) {
+          tempRows.splice(index, 1);
+        } else{
+          tempRows.push(row)
+        }
+        tempRowNodes[row] = !tempRowNodes[row]
+        setRowNodes(tempRowNodes)
+        setFilteredRowNodes(tempRows)
+    }
 }
   
   const renderOutputCategory = () => {
@@ -94,66 +171,42 @@ export default function ModelResults(props) {
       else {
         return (
           <Box style={{backgroundColor:'white'}} sx={{m:3, padding:2, boxShadow:3}}>
-            <h3>{ParetoDictionary[props.category] ? ParetoDictionary[props.category] : props.category}</h3>
-            <TableContainer sx={{overflowX:'auto'}}>
-            <Table style={{border:"1px solid #ddd"}} size='small'>
-              <TableHead style={{backgroundColor:"#6094bc", color:"white"}}>
-              <TableRow>
-              {props.category === "v_F_Overview_dict" ? 
-              <>
-                <TableCell style={{backgroundColor:"#6094bc", color:"white"}}>KPI</TableCell> 
-                <TableCell style={{backgroundColor:"#6094bc", color:"white"}}>Units</TableCell> 
-                <TableCell style={{backgroundColor:"#6094bc", color:"white"}}>Value</TableCell> 
-              </>
-              :
-              props.scenario.results.data[props.category][0].map((value, index) => {
-                return <TableCell style={{backgroundColor:"#6094bc", color:"white"}}>{value}</TableCell>
-              })
-              }
-              
-              </TableRow>
-              </TableHead>
-              {props.category === "v_F_Overview_dict" ? 
-              <TableBody>
-              {props.scenario.results.data[props.category].slice(1).map((value, index) => {
-                return (<TableRow>
-                {value.map((cellValue, i)=> {
-                  return (i !== 1 &&
-                     <TableCell 
-                      align={(i === (value.length - 1)) ? "right" : "left"} 
-                      key={"" + index + i} 
-                      style={i === 0 ? styles.firstCol : styles.other}>
-                        {(i === (value.length - 1)) ? 
-                        cellValue.toLocaleString('en-US', {maximumFractionDigits:0}) : 
-                        cellValue ? cellValue.replace('v_F_','').replace('v_C_','Cost ')
-                        : null
-                      }
-                  </TableCell>
-                  )
-                  
-                })}
-                </TableRow>)
-              })}
-              </TableBody> 
-              : 
-              <TableBody>
-              {props.scenario.results.data[props.category].slice(1).map((value, index) => {
-                return (<TableRow>
-                {value.map((cellValue, i)=> {
-                  return <TableCell 
-                          align={(i === (value.length - 1)) ? "right" : "left"} 
-                          key={"" + index + i} 
-                          style={i === 0 ? styles.firstCol : styles.other}>
-                            {cellValue.toLocaleString('en-US', {maximumFractionDigits:0})}
-                          </TableCell>
-                })}
-                </TableRow>)
-              })}
-              </TableBody>
-              }
-            
-            </Table>
-            </TableContainer>
+          <Grid container>
+            <Grid item xs={11.5}>
+              <DataTable 
+                section="output"
+                scenario={scenario}
+                setScenario={setScenario}
+                columnNodesMapping={columnNodesMapping}
+                columnNodes={columnNodes}
+                filteredColumnNodes={filteredColumnNodes}
+                rowNodesMapping={rowNodesMapping}
+                rowNodes={rowNodes}
+                filteredRowNodes={filteredRowNodes}
+                category={props.category}
+                handleEditInput={props.handleEditInput}
+                data={props.scenario.results.data}
+              />
+            </Grid>
+            <Grid item xs={0.5}>
+            <Box sx={{display: 'flex', justifyContent: 'flex-end', marginLeft:'10px'}}>
+            <FilterDropdown
+                width="300px"
+                maxHeight="300px"
+                option1="Column"
+                filtered1={filteredColumnNodes}
+                total1={columnNodesMapping}
+                isAllSelected1={isAllColumnsSelected}
+                handleFilter1={handleColumnFilter}
+                option2="Row"
+                filtered2={filteredRowNodes}
+                total2={rowNodesMapping}
+                isAllSelected2={isAllRowsSelected}
+                handleFilter2={handleRowFilter}
+            />
+            </Box>
+          </Grid>
+          </Grid>
           </Box>
         )
       }
