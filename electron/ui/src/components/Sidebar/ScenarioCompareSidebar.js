ab4cd4682cb322b85ba0e06d059b192e158e3958
MichaelPesce <michaelcpesce@gmail.com>
2023-06-12 13:33:47 -0500

    updated styling for sidebar on compare page; if well received, can apply this styling to main sidebar

--- electron/ui/src/components/Sidebar/ScenarioCompareSidebar.js
+++ electron/ui/src/components/Sidebar/ScenarioCompareSidebar.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { Box, Drawer, CssBaseline, Collapse, Divider, Tooltip } from '@mui/material'
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import { Box, Drawer, CssBaseline, Collapse, Button, Tooltip, IconButton } from '@mui/material'
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import ParetoDictionary from '../../assets/ParetoDictionary.json'
@@ -19,14 +18,45 @@ export default function Sidebar(props) {
     topLevelCategory: {
       paddingLeft: "0px",
       fontWeight: "500",
-
+      margin: 0,
     },
     subcategory: {
       paddingLeft: "10px",
+      margin: 0,
     },
     inputDifference: {
-      backgroundColor: "rgb(255,215,0, 0.5)",
+      backgroundColor: "rgb(255,215,0, 0.4)",
+      cursor: "pointer",
+      padding: 10,
+      marginLeft: "10px",
+      marginRight: "10px",
+      borderRadius: "5px",
+      textAlign: "left",
+      marginTop: 1,
+      marginBottom: 1
     },
+    selected: {
+      cursor: "pointer",
+      color:"#0b89b9",
+      backgroundColor: "#D4EFFF",
+      padding: 10,
+      marginLeft: "10px",
+      marginRight: "10px",
+      borderRadius: "5px",
+      textAlign: "left",
+      marginTop: 1,
+      marginBottom: 1
+    },
+    unselected: {
+      cursor: "pointer",
+      padding: 10,
+      marginLeft: "10px",
+      marginRight: "10px",
+      borderRadius: "5px",
+      textAlign: "left",
+      marginTop: 1,
+      marginBottom: 1
+    }
   }
 
   const handleClick = (key) => {
@@ -37,68 +67,64 @@ export default function Sidebar(props) {
 
   const handleCheckForDifference = (key) => {
     try {
-      if (key === "Static" || key === "Dynamic") {
-        for (let each of Subcategories[key]) {
-          if(Object.keys(deltaDictionary[each]).length > 0) return styles.inputDifference
-        }
-        return {backgroundColor: "white"}
-      }
+      if(category === key) return styles.selected
       else {
-        if(Object.keys(deltaDictionary[key]).length > 0) return styles.inputDifference
-        else return {backgroundColor: "white"}
+        if (key === "Static" || key === "Dynamic") {
+          for (let each of Subcategories[key]) {
+            if(Object.keys(deltaDictionary[each]).length > 0) return styles.inputDifference
+          }
+          return styles.unselected
+        }
+        else {
+          if(Object.keys(deltaDictionary[key]).length > 0) return styles.inputDifference
+          else return styles.unselected
+        }
       }
+      
     }
     catch(e) {
-      return {backgroundColor: "white"}
+      return styles.unselected
     }
     
   }
 
   const renderTopLevelCategories = () => {
       return (
-        <>
-        <Fragment>
-          <ListItem disablePadding>
-              <ListItemButton selected={category==="output"} onClick={() => handleClick("output")}>
-              <ListItemText primaryTypographyProps={styles.topLevelCategory} primary={"Output"} />
-              </ListItemButton>
-          </ListItem>
-          <Divider></Divider>
-        </Fragment>
-        <ListItem key={"listitem_dynamic"} disablePadding>
-              <ListItemButton key={"listitembutton_dynamic"} selected={false} style={handleCheckForDifference("Dynamic")} onClick={() => setOpenDynamic(!openDynamic)}>
-              <ListItemText primaryTypographyProps={styles.topLevelCategory} key={"listitemtext_dynamic"} primary={"Dynamic Inputs"} />
-              {openDynamic ? <ExpandLess /> : <ExpandMore />}
-              </ListItemButton>
-          </ListItem>
-          <Divider key={"divider_dynamic"}></Divider>
+        <div>
+          <div style={category==="output" ? styles.selected : styles.unselected} onClick={() => handleClick("output")}> 
+              <p style={styles.topLevelCategory}>Output</p>
+          </div>
+          <div style={handleCheckForDifference("Dynamic")}  onClick={() => setOpenDynamic(!openDynamic)}> 
+              <p style={styles.topLevelCategory}>
+                
+                <IconButton disableRipple edge={"end"} size="small" sx={{marginTop: -3, marginBottom: -3}}>{openDynamic ? <ExpandLess /> : <ExpandMore />}</IconButton>
+                Dynamic Inputs
+              </p>
+          </div>
           {renderDynamicCategories()}
-          <ListItem key={"listitem_static"} disablePadding>
-              <ListItemButton key={"listitembutton_static"} selected={false} style={handleCheckForDifference("Static")} onClick={() => setOpenStatic(!openStatic)}>
-              <ListItemText primaryTypographyProps={styles.topLevelCategory} key={"listitemtext_static"} primary={"Static Inputs"} />
-              {openStatic ? <ExpandLess /> : <ExpandMore />}
-              </ListItemButton>
-          </ListItem>
-          <Divider key={"divider_static"}></Divider>
+          <div style={handleCheckForDifference("Static")}  onClick={() => setOpenStatic(!openStatic)}> 
+              <p style={styles.topLevelCategory}>
+                
+                <IconButton disableRipple edge={"end"} size="small" sx={{marginTop: -3, marginBottom: -3}}>{openStatic ? <ExpandLess /> : <ExpandMore />}</IconButton>
+                Static Inputs
+              </p>
+          </div>
           {renderStaticCategories()}
-          </>
+        </div>
       ) 
   }
 
   const renderDynamicCategories = () => {
     return (
       <Collapse in={openDynamic} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
       {Subcategories.Dynamic.map( (value,index) => {
         return(
-          <Fragment key={"_"+index}>
+          <div>
             <Tooltip title={ParetoDictionary[value] ? ParetoDictionary[value] : CategoryNames[value] ? CategoryNames[value] : value} placement="right-start">
-            <ListItem key={"listitem_"+value} disablePadding>
-                <ListItemButton key={"listitembutton_"+value} selected={category===value} style={handleCheckForDifference(value)} onClick={() => handleClick(value)}>
-                <ListItemText 
-                  style={styles.subcategory}
-                  key={"listitemtext_"+value} 
-                  primary={CategoryNames[value] ? CategoryNames[value] :
+            {/* <div style={category===value ? styles.selected : styles.unselected} onClick={() => handleClick(value)}>  */}
+            <div style={handleCheckForDifference(value)} onClick={() => handleClick(value)}>
+                <p style={styles.subcategory}>
+                {CategoryNames[value] ? CategoryNames[value] :
                     value.replace('_dict','')
                           .replace('v_F_','')
                           .replace('v_C_','Cost ')
@@ -109,16 +135,14 @@ export default function Sidebar(props) {
                           .replace('v_X_','Storage ')
                           .replace('v_T_','Treatment ')
                           .replace('vb_y_Flow','Directional Flow')
-                          .replace('vb_y_','New ')} 
-                />
-                </ListItemButton>
-            </ListItem>
+                          .replace('vb_y_','New ')
+                  } 
+                </p>
+            </div>
             </Tooltip>
-            <Divider key={"divider_"+value}></Divider>
-          </Fragment>
+          </div>
         )
       })}
-     </List>
       </Collapse>
     )
   }
@@ -126,17 +150,13 @@ export default function Sidebar(props) {
   const renderStaticCategories = () => {
     return (
       <Collapse in={openStatic} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
       {Subcategories.Static.map( (value,index) => {
-        return (
-          <Fragment key={"_"+index}>
+        return(
+          <div>
             <Tooltip title={ParetoDictionary[value] ? ParetoDictionary[value] : CategoryNames[value] ? CategoryNames[value] : value} placement="right-start">
-            <ListItem style={{fontWeight:'bold'}} key={"listitem_"+value} disablePadding>
-                <ListItemButton key={"listitembutton_"+value} selected={category===value} style={handleCheckForDifference(value)} onClick={() => handleClick(value)}>
-                <ListItemText 
-                  style={styles.subcategory}
-                  key={"listitemtext_"+value} 
-                  primary={CategoryNames[value] ? CategoryNames[value] :
+            <div style={handleCheckForDifference(value)} onClick={() => handleClick(value)}>
+                <p style={styles.subcategory}>
+                {CategoryNames[value] ? CategoryNames[value] :
                     value.replace('_dict','')
                           .replace('v_F_','')
                           .replace('v_C_','Cost ')
@@ -147,16 +167,14 @@ export default function Sidebar(props) {
                           .replace('v_X_','Storage ')
                           .replace('v_T_','Treatment ')
                           .replace('vb_y_Flow','Directional Flow')
-                          .replace('vb_y_','New ')} 
-                />
-                </ListItemButton>
-            </ListItem>
+                          .replace('vb_y_','New ')
+                  } 
+                </p>
+            </div>
             </Tooltip>
-            <Divider key={"divider_"+value}></Divider>
-          </Fragment>
+          </div>
         )
       })}
-     </List>
       </Collapse>
     )
   }
@@ -171,7 +189,7 @@ export default function Sidebar(props) {
           sx={{
             width: drawerWidth,
             flexShrink: 0,
-            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+            [`& .MuiDrawer-paper`]: { backgroundColor:"#F5F5F6", width: drawerWidth, boxSizing: 'border-box' },
             [`& .MuiBox-root`]: {marginBottom: '60px' },
           }}
           PaperProps={{
@@ -180,16 +198,19 @@ export default function Sidebar(props) {
               // marginTop: '71px',
               // paddingBottom: '71px'
               marginTop: '152px',
-              paddingBottom: '152px'
+              paddingBottom: '102px'
               }
           }}
           open={open}
         >
           <Box key="drawer_box" sx={{ overflow: 'auto', overflowX: 'hidden'}}>
+              {renderTopLevelCategories()}
+          </Box>
+          {/* <Box key="drawer_box" sx={{ overflow: 'auto', overflowX: 'hidden'}}>
               <List key="drawer_list" aria-label="sidebar_table" sx={{paddingTop:'0px'}}>
               {renderTopLevelCategories()}
             </List>
-          </Box>
+          </Box> */}
         </Drawer>
       </Box>
     }
