fd5fc7d6a5e83d616d8c0bd0f3ae353f1f7744be
MichaelPesce <michaelcpesce@gmail.com>
2023-08-02 08:32:08 -0400

    added last page/first page arrows for tables with pagination

--- electron/ui/src/components/OverrideTable/OverrideTable.js
+++ electron/ui/src/components/OverrideTable/OverrideTable.js
@@ -1,7 +1,76 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import { Table, TableCell, TableHead, TableRow, TableContainer, TablePagination, Button } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import { Table, TableCell, TableHead, TableRow, TableFooter, TableContainer, TablePagination, Button, IconButton, Box } from '@mui/material';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
 import { INFRASTRUCTURE_CAPEX_MAPPING, VARIABLE_INDEXES }  from '../../assets/InfrastructureCapexMapping.js'
 import OverrideTableRows from './OverrideTableRows';
+
+
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 
 export default function OverrideTable(props) {  
@@ -216,6 +285,27 @@ const renderOutputTable = () => {
                 setNewInfrastructureOverrideRow={setNewInfrastructureOverrideRow}
                 addNewRow={addNewRow}
               />
+              <TableFooter>
+                <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[25, 50, 100]}
+                  // colSpan={3}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  // component="div"
+                  // SelectProps={{
+                  //   inputProps: {
+                  //     'aria-label': 'rows per page',
+                  //   },
+                  //   native: true,
+                  // }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+                </TableRow>
+              </TableFooter>
             
             </Table>
             {category === "vb_y_overview_dict" && 
@@ -223,16 +313,6 @@ const renderOutputTable = () => {
                 + Add infrastructure override
               </Button> 
             }
-            
-            <TablePagination
-              rowsPerPageOptions={[25, 50, 100]}
-              component="div"
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
             
             </TableContainer>
             
