a9b50be184188ff3f948b3849527874aef6374b5
MichaelPesce <michaelcpesce@gmail.com>
2023-12-05 18:57:07 -0600

    add link for workshop baseline input

--- electron/ui/src/components/FileUploadModal/FileUploadModal.js
+++ electron/ui/src/components/FileUploadModal/FileUploadModal.js
@@ -19,6 +19,7 @@ export default function Dashboard(props) {
     const fileTypes = ["xlsx"];
     // const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/0.6_rel/pareto/case_studies/strategic_small_case_study.xlsx"
     const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/main/pareto/case_studies/strategic_permian_demo.xlsx"
+    const workshopFileUrl = "https://github.com/project-pareto/project-pareto/raw/main/pareto/case_studies/workshop_baseline_all_data.xlsx"
 
 
   useEffect(()=>{
@@ -183,6 +184,10 @@ export default function Dashboard(props) {
         <Grid item xs={6}>
             {showWarning && <p style={{color:'red'}}>{warningMessage}</p>}
         </Grid>
+        <Grid item xs={6}>
+            <p ><a data-cy="excel-download" style={styles.sampleFile} href={workshopFileUrl} download>Download Workshop Input File</a></p>
+        </Grid>
+        <Grid item xs={6}></Grid>
         <Grid item xs={12}>
             <Button id="create-scenario-button" style={styles.button} onClick={handleCreateScenario}>Create Scenario</Button>
         </Grid>
