3fcb2792963e4832370e931bf3c4adc4ecb82caa
MichaelPesce <michaelcpesce@gmail.com>
2024-01-17 14:38:18 -0600

    use 0.9.0_rel excel file for sample download file

--- electron/ui/src/components/FileUploadModal/FileUploadModal.js
+++ electron/ui/src/components/FileUploadModal/FileUploadModal.js
@@ -17,8 +17,8 @@ export default function FileUploadModal(props) {
     const [ warningMessage, setWarningMessage ] = useState("")
     const [ file, setFile ] = useState(null)
     const fileTypes = ["xlsx", "kmz", "kml"];
-    // const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/0.6_rel/pareto/case_studies/strategic_small_case_study.xlsx"
-    const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/main/pareto/case_studies/strategic_permian_demo.xlsx"
+    // const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/main/pareto/case_studies/strategic_permian_demo.xlsx"
+    const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/0.9_rel/pareto/case_studies/strategic_permian_demo.xlsx"
 
 
   useEffect(()=>{
