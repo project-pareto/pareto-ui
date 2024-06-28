04816afd0124fa5f7863437983cedc4dbd1e2ab9
MichaelPesce <michaelcpesce@gmail.com>
2024-06-28 12:56:01 -0400

    issues with port crawling, just use 50011 for now

--- electron/ui/src/AppContext.js
+++ electron/ui/src/AppContext.js
@@ -8,38 +8,38 @@ export const useApp = () => {
 
 export const AppProvider = ({ children }) => {
   const [ port, setPort ] = useState(50011);
-  const [ foundPort, setFoundPort ] = useState(false)
+  const [ foundPort, setFoundPort ] = useState(true)
 
-  useEffect(() => {
-    /*
-      search for backend port
-    */
-    fetch('http://localhost:'+port+'/get_project_name', {
-        method: 'GET', 
-        mode: 'cors',
-    })
-    .then(response => {
-        if (response.status === 200) {
-            response.json()
-            .then((data)=>{
-                if (data === "pareto") { 
-                    // console.log("found pareto on port "+ port)
-                    setFoundPort(true)
-                }
-                else  {
-                    setPort(port + 1)
-                }
-            }).catch((err)=>{
-                setPort(port + 1)
-            })
-        }
-        else {
-            setPort(port + 1)
-        }
-    }).catch((err) => {
-        setPort(port + 1)
-    })
-  }, [port]);
+  // useEffect(() => {
+  //   /*
+  //     search for backend port
+  //   */
+  //   fetch('http://localhost:'+port+'/get_project_name', {
+  //       method: 'GET', 
+  //       mode: 'cors',
+  //   })
+  //   .then(response => {
+  //       if (response.status === 200) {
+  //           response.json()
+  //           .then((data)=>{
+  //               if (data === "pareto") { 
+  //                   // console.log("found pareto on port "+ port)
+  //                   setFoundPort(true)
+  //               }
+  //               else  {
+  //                   setPort(port + 1)
+  //               }
+  //           }).catch((err)=>{
+  //               setPort(port + 1)
+  //           })
+  //       }
+  //       else {
+  //           setPort(port + 1)
+  //       }
+  //   }).catch((err) => {
+  //       setPort(port + 1)
+  //   })
+  // }, [port]);
 
 
   const value = {
