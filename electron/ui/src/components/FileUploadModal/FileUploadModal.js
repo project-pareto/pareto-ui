d0b33b94a827178c1b3b53c81fd3ec4bff3b46fa
MichaelPesce <michaelcpesce@gmail.com>
2024-07-10 07:10:23 -0400

    revert hard coded main

--- electron/ui/src/components/FileUploadModal/FileUploadModal.js
+++ electron/ui/src/components/FileUploadModal/FileUploadModal.js
@@ -20,11 +20,9 @@ export default function FileUploadModal(props) {
     const [ file, setFile ] = useState(null)
     const fileTypes = ["xlsx", "kmz", "kml"];
 
-    let tempVersion = "main"
-
-    const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/"+tempVersion+"/pareto/case_studies/strategic_permian_demo.xlsx"
-    const workshopFileUrl = "https://github.com/project-pareto/project-pareto/raw/"+tempVersion+"/pareto/case_studies/workshop_baseline_all_data.xlsx"
-    const workshopFileName = "workshop_baseline_all_data_"+tempVersion+".xlsx"
+    const sampleFileUrl = "https://github.com/project-pareto/project-pareto/raw/"+process.env.REACT_APP_PARETO_VERSION+"/pareto/case_studies/strategic_permian_demo.xlsx"
+    const workshopFileUrl = "https://github.com/project-pareto/project-pareto/raw/"+process.env.REACT_APP_PARETO_VERSION+"/pareto/case_studies/workshop_baseline_all_data.xlsx"
+    const workshopFileName = "workshop_baseline_all_data_"+process.env.REACT_APP_PARETO_VERSION+".xlsx"
     
   const { port } = useApp()
 
