0487e1dd2389e4c761a8e328dc19f300c289fa33
MichaelPesce <michaelcpesce@gmail.com>
2026-01-13 13:05:08 +0000

    fix header test

--- electron/ui/src/tests/header.test.tsx
+++ electron/ui/src/tests/header.test.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header/Header"
 import mockScenarios from './data/mockScenarios.json'
 import mockScenario from './data/mockScenario.json'
 import * as React from 'react'
+import { BrowserRouter } from "react-router-dom";
 
 const mockFunction = () => {
     console.log("function");
@@ -12,14 +13,14 @@ const mockIndex = 1;
 
 
 test('test header', () => {
-    render( <Header 
+    render( <BrowserRouter><Header 
             scenarios={mockScenarios} 
             scenarioData={mockScenario} 
             index={mockIndex} 
             handleNewScenario={mockFunction} 
             handleSelection={mockFunction}
             showHeader={true}
-            /> )
+            /></BrowserRouter> )
 
     expect(screen.getByRole('img', {  name: /Pareto logo/i})).toBeInTheDocument();
     // expect(screen.getByRole('button', {  name: /case study 2/i})).toBeInTheDocument();
