33cbede8d04e3c52f55a20f56968e9f69170bfe5
MichaelPesce <michaelcpesce@gmail.com>
2026-02-26 10:01:49 -0600

    update sidebar test

--- electron/ui/src/tests/sidebar.test.tsx
+++ electron/ui/src/tests/sidebar.test.tsx
@@ -3,22 +3,28 @@ import Sidebar from "../components/Sidebar/Sidebar"
 import mockScenario from './data/mockScenario.json'
 import * as React from 'react'
 
+jest.mock("../context/MapContext", () => ({
+    useMapValues: () => ({
+        selectedNode: null,
+        showNetworkNode: false,
+        showNetworkPipeline: false,
+    }),
+}));
+
 const mockFunction = () => {
     console.log("function");
 };
 
-const mockSelected = 0;
 const mockCategory = null;
 
 
 test('test sidebar', () => {
     render( <Sidebar 
-            selected={mockSelected} 
             scenario={mockScenario}
+            section={0}
             category={mockCategory}
             handleSetCategory={mockFunction}
             /> )
 
-    // expect(screen.getByRole('list', {  name: /sidebar_table/i})).toBeInTheDocument();
 
 })
